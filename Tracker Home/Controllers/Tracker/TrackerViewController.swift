a90b5b595b55542b4d95ca3d473e2feca2074682
Blanxxty <mvtvr21@yandex.ru>
2024-02-01 17:55:19 +0300

    teset 15_sprint, я уже схожу с ума, вроде все сделал, над тем как сохранять кнопку и кол-во выполненных дней 2 дня сидел, решение само в голоу пришло, за 15 минут сделал

--- Tracker Home/Controllers/Tracker/TrackerViewController.swift
+++ Tracker Home/Controllers/Tracker/TrackerViewController.swift
@@ -261,6 +261,16 @@ extension TrackerViewController: UISearchTextFieldDelegate {
         collectionView.reloadData()
         return true
     }
+    
+    //динамическое обновление
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if let currentText = (textField.text as NSString?)?.replacingCharacters(in: range, with: string) {
+            print("Текущий текст: \(currentText)")
+            setupPlugView()
+            collectionView.reloadData()
+        }
+        return true
+    }
 }
 
 //MARK: UICollectionViewCell - Header
